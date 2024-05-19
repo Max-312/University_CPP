5b9da4d608d374aa1242077005ce52105b6d4c37
Max-312 <137430055+Max-312@users.noreply.github.com>
2024-05-19 23:14:29 +0300

    task9

--- huffman.h
+++ huffman.h
@@ -19,6 +19,8 @@ struct CompareNodes {
     }
 };
 
+void encodeHelper(TreeNode* node, std::string code, std::map<char, std::string>& codes);
+
 std::string encode(const std::string& text);
 std::string decode(const std::string& encoded, std::map<char, std::string>& codes);
 
